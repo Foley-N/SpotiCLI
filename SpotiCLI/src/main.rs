d0c59e2cac70691d10a884d6f6f367b7d76bf06b
Foley-N <noahfoleypro@gmail.com>
2024-12-22 16:34:56 -0600

    added.env support for the client ID and SECRET and started working on getting auth token and wokring on search functions.

--- SpotiCLI/src/main.rs
+++ SpotiCLI/src/main.rs
@@ -1,10 +1,14 @@
 use reqwest;
+use dotenv;
 
 fn main() {
+    Ok(dotenv); // loads .env
     let baseUrl = "https://api.spotify.com/v1/";
-    println!(baseUrl);
+    println!("{}", baseUrl);
 }
 
+fn get_token()
+
 fn search_album(){
-    
+    format!("https://api.spotify.com/v1/albums")
 }
